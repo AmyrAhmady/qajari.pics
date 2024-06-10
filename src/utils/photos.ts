8978198c87d6f12bc43336eff547aa9781ba3082
iAmir <hhm6@yahoo.com>
2024-06-10 08:27:32 +0330

    new image url, different approach for thumbnails

--- src/utils/photos.ts
+++ src/utils/photos.ts
@@ -1,30 +1,29 @@
 import { IFolder, IPhoto } from "./@types";
 import data from "./data.json";
 
-const IMAGE_HOST_BASE_URL = "https://qajari.pics/";
-const breakpoints = [1080, 640, 384, 256, 128, 96, 64, 48];
+const IMAGE_HOST_BASE_URL = "http://assets.qajari.pics/";
 const photoAlbums: IFolder[] = data;
-
-const thumbnailLink = (data: IPhoto, width: number) =>
-  `${data.thumbnail}${width}`;
 
 export const getFolderPhotos = (folder: IFolder) => {
   return folder.items.map((photo) => {
-    const width = breakpoints[0];
-    const height = (photo.height / photo.width) * width;
-
     return {
       src: encodeURI(IMAGE_HOST_BASE_URL + `${folder.name}/${photo.name}`),
+      width: photo.width,
+      height: photo.height,
+    };
+  });
+};
+
+export const getFolderThumbnails = (folder: IFolder) => {
+  return folder.items.map((photo) => {
+    const width = 360;
+    const height = Math.round((photo.height / photo.width) * width);
+    return {
+      src: encodeURI(
+        IMAGE_HOST_BASE_URL + `thumbnails/${folder.name}/${photo.name}`
+      ),
       width,
       height,
-      srcSet: breakpoints.map((breakpoint) => {
-        const height = Math.round((photo.height / photo.width) * breakpoint);
-        return {
-          src: thumbnailLink(photo, breakpoint),
-          width: breakpoint,
-          height,
-        };
-      }),
     };
   });
 };
